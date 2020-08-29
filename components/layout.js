8b62b3f044591e350b805ba3a4214efd99d09fcb
8smorefun <8smorefun@gmail.com>
2020-08-29 10:45:17 +0000

    change intro, title and description

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = '8smorefun'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Next.js Blog'
 
 export default function Layout({ children, home }) {
   return (
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Learn how to build a personal blog website using Next.js"
         />
         <meta
           property="og:image"
