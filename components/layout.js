fafbed1d8656440fbff4a0074d278938325b6d75
8smorefun <8smorefun@gmail.com>
2020-08-29 14:26:38 +0000

    added excerpt on homepage, on post meta description, og and twitter

--- components/layout.js
+++ components/layout.js
@@ -5,6 +5,7 @@ import Link from 'next/link'
 
 const name = '8smorefun'
 export const siteTitle = 'Next.js Blog'
+export const siteDescription = 'Learn how to build a personal blog website using Next.js'
 
 export default function Layout({ children, home }) {
   return (
@@ -14,16 +15,22 @@ export default function Layout({ children, home }) {
         <title>{siteTitle}</title>
         <meta
           name="description"
-          content= "Learn how to build a personal blog website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content= {siteDescription}
         />
         <meta name="og:title" content={siteTitle} />
+        <meta name="og:description" content={siteDescription} />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(siteTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={siteTitle} />
+        <meta name="twitter:description" content={siteDescription} />
+        <meta 
+          name="twitter:image"
+          content={`https://og-image.now.sh/${encodeURI(siteTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} 
+        />
       </Head>
       <header className={styles.header}>
         {home ? (
